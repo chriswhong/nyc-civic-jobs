401c4470a3bc61fbe95e0cf90c38f5f93a7f73d2
Chris Whong <chris@whong.org>
2018-11-11 11:19:56 -0500

    add salary range

--- frontend/src/pages/agency.js
+++ frontend/src/pages/agency.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import moment from 'moment'
+import numeral from 'numeral'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+
+const formatCurrency = (number) => {
+  return numeral(number).format('($0a)')
+}
 
 export default class AgencyPage extends React.Component {  
   constructor(props) {
@@ -55,11 +60,12 @@ export default class AgencyPage extends React.Component {
       
       return (
         <Layout>
+          <Link to="/">Back to Agency Listing</Link>
           <h3>Showing {count} job listings for {agency}</h3>
           <p>Click a job to view the full listing on NYC&apos;s offical jobs website</p>
           <div className="list-group">
             {jobs.map(job => {
-              const { job_id, business_title, job_category, posting_date } = job;
+              const { job_id, business_title, job_category, posting_date, salary_range_from, salary_range_to } = job;
               const date_string = moment(posting_date).fromNow()
               
               const url = `https://a127-jobs.nyc.gov/psc/nycjobs/EMPLOYEE/HRMS/c/HRS_HRAM.HRS_APP_SCHJOB.GBL?Page=HRS_APP_JBPST&Action=U&FOCUS=Applicant&SiteId=1&JobOpeningId=${job_id}&PostingSeq=1`
@@ -71,6 +77,7 @@ export default class AgencyPage extends React.Component {
                     <div>
                       <h5 className="mb-1">{business_title}</h5>
                       <small>Division: {job.division_work_unit}</small> 
+                      <p><small>Compensation: {formatCurrency(salary_range_from)} - {formatCurrency(salary_range_to)}</small></p>
                     </div>
                     <small>{date_string}</small>
                   </div>
