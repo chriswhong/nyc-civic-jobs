cb49681ab469233a8162ca07754a74c27768ba64
Chris Whong <chris@whong.org>
2018-11-17 10:49:38 -0500

    add api route for individual job

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-var jobs = require('./routes/jobs');
+const jobs = require('./routes/jobs');
 
 require('dotenv').config();
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
@@ -34,11 +33,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.use('/jobs', jobs);
 
-app.get('/agency/*', function( req, res ) {
+app.get('/agency/*', (req, res) => {
   res.sendfile(path.join(__dirname, 'public/agency', 'index.html'));
 });
 
-app.get('/category/*', function( req, res ) {
+app.get('/category/*', (req, res) => {
   res.sendfile(path.join(__dirname, 'public/category', 'index.html'));
 });
 
@@ -54,7 +53,7 @@ app.get('/category/*', function( req, res ) {
 //   // set locals, only providing error in development
 //   res.locals.message = err.message;
 //   res.locals.error = req.app.get('env') === 'development' ? err : {};
-// 
+//
 //   // render the error page
 //   res.status(err.status || 500);
 //   res.render('error');
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 const mongoose = require('mongoose');
 
 const jobSchema = require('../schema/job');
-const { LookupCategoryDisplayName } = require('../utils/process-categories')
+const { LookupCategoryDisplayName } = require('../utils/process-categories');
 
 // fields to return
 const fields = 'job_id agency agency_id business_title division_work_unit job_category_ids posting_date salary_range_from salary_range_to salary_frequency job_description';
@@ -14,27 +15,27 @@ const posting_type = 'External';
 
 
 // get counts by agency and category
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   const promises = [
     Job.aggregate([
       { $match: { posting_type } },
       {
         $group: {
-            _id: '$agency_id',  //$region is the column name in collection
-            displayName : { $first: '$agency' },
-            count: {$sum: 1}
-        }
+          _id: '$agency_id', // $region is the column name in collection
+          displayName: { $first: '$agency' },
+          count: { $sum: 1 },
+        },
       },
       { $sort: { count: -1 } },
     ]).exec(),
     Job.aggregate([
       { $match: { posting_type } },
-      { $unwind: "$job_category_ids" },
+      { $unwind: '$job_category_ids' },
       {
         $group: {
-            _id: '$job_category_ids',  //$region is the column name in collection
-            count: {$sum: 1}
-        }
+          _id: '$job_category_ids', // $region is the column name in collection
+          count: { $sum: 1 },
+        },
       },
       { $sort: { count: -1 } },
     ]).exec(),
@@ -48,7 +49,7 @@ router.get('/', function(req, res, next) {
         _id,
         displayName,
         count,
-      }
+      };
     });
 
     res.send({
@@ -59,10 +60,10 @@ router.get('/', function(req, res, next) {
 });
 
 // get jobs by agency
-router.get('/agency/:agency_id', function(req, res, next) {
+router.get('/agency/:agency_id', (req, res, next) => {
   const { agency_id } = req.params;
 
-  Job.find({ agency_id, posting_type }, fields, {sort: {posting_date: 'desc'}}, (err, jobs) => {
+  Job.find({ agency_id, posting_type }, fields, { sort: { posting_date: 'desc' } }, (err, jobs) => {
     if (err) return handleError(err);
 
     res.send(jobs);
@@ -70,17 +71,26 @@ router.get('/agency/:agency_id', function(req, res, next) {
 });
 
 // get jobs by category
-router.get('/category/:category_id', function(req, res, next) {
+router.get('/category/:category_id', (req, res, next) => {
   const { category_id } = req.params;
 
-  Job.find({ job_category_ids: category_id, posting_type }, fields, {sort: {posting_date: 'desc'}}, (err, jobs) => {
+  Job.find({ job_category_ids: category_id, posting_type }, fields, { sort: { posting_date: 'desc' } }, (err, jobs) => {
     if (err) return handleError(err);
 
     res.send(jobs);
   });
 });
 
+// get one job
+router.get('/id/:job_id', (req, res, next) => {
+  const { job_id } = req.params;
 
+  Job.findOne({ job_id }, fields, (err, job) => {
+    if (err) return handleError(err);
+
+    res.send(job);
+  });
+});
 
 
 module.exports = router;
