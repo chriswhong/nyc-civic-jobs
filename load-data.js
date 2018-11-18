07713aee2676223f1b1170bb1cb340bf6a4b5fa8
Chris Whong <chris@whong.org>
2018-11-17 21:12:36 -0500

    add updatedat metadata to database

--- load-data.js
+++ load-data.js
@@ -1,6 +1,6 @@
-const request = require("request");
+const fetch = require('node-fetch');
 const mongoose = require('mongoose');
-const slugify = require('slugify')
+const slugify = require('slugify');
 
 const jobSchema = require('./schema/job');
 const agencyLookup = require('./utils/agency-lookup');
@@ -8,28 +8,33 @@ const { processCategories } = require('./utils/process-categories');
 
 require('dotenv').config();
 
-const apiCall = 'https://data.cityofnewyork.us/resource/swhp-yxa4.json?$limit=10000';
+const dataApiCall = 'https://data.cityofnewyork.us/resource/swhp-yxa4.json?$limit=10000';
+const metaApiCall = 'https://data.cityofnewyork.us/api/views/metadata/v1/kpav-sd4t';
 
-const replaceOddCharacters = (string) => {
-  return string
-    .replace(/â€œ/g, '“')
-    .replace(/â€/g, '”')
-    .replace(/â€™/g, '’')
-    .replace(/â€˜/g, '‘')
-    .replace(/â€”/g, '–')
-    .replace(/â€“/g, '—')
-    .replace(/â€¢/g, '-')
-    .replace(/â€¦/g, '…');
-};
+const replaceOddCharacters = string => string
+  .replace(/â€œ/g, '“')
+  .replace(/â€/g, '”')
+  .replace(/â€™/g, '’')
+  .replace(/â€˜/g, '‘')
+  .replace(/â€”/g, '–')
+  .replace(/â€“/g, '—')
+  .replace(/â€¢/g, '-')
+  .replace(/â€¦/g, '…');
 
-request({
-  url: apiCall,
-}, (err, response, rawJSON) => {
+(async () => {
+  const response = await fetch(dataApiCall)
+    .then(d => d.text())
+    .catch(err => console.error(err));
+
+  const { dataUpdatedAt } = await fetch(metaApiCall)
+    .then(d => d.json())
+    .catch(err => console.error(err));
+
   // replace oddly-encoded characters in the raw data
-  const cleanJSON = JSON.parse(replaceOddCharacters(rawJSON));
+  const cleanJSON = JSON.parse(replaceOddCharacters(response));
 
   // add clean agency_id to each record
-  jobsWithAgencyData = cleanJSON.map((job) => {
+  const jobsWithAgencyData = cleanJSON.map((job) => {
     const { agency, job_category } = job;
 
     const { displayName } = agencyLookup(agency);
@@ -39,41 +44,51 @@ request({
     // convert job_categories to array
     // add clean category slug to each record
     if (job_category) {
-      console.log(processCategories)
+      console.log(processCategories);
       job.job_category_ids = processCategories(job_category);
     } else {
-      job.job_category_ids = ['no-category']
+      job.job_category_ids = ['no-category'];
     }
 
     return job;
   });
 
+  console.log(jobsWithAgencyData, dataUpdatedAt)
+
   // make a connection
   mongoose.connect(process.env.MONGO_URI);
 
   // get reference to database
-  var db = mongoose.connection;
+  const db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'connection error:'));
 
-  db.once('open', function() {
-      console.log("Connection Successful!");
+  db.once('open', () => {
+    console.log('Connection Successful!');
 
-      // compile schema to model
-      var Job = mongoose.model('Job', jobSchema, 'jobs');
+    // compile schema to model
+    const Job = mongoose.model('Job', jobSchema, 'jobs');
 
-      // delete everything in the collection
-      Job.deleteMany({}, () => {
-        console.log('Deleted all documents in Collection')
+    // delete everything in the collection
+    Job.deleteMany({}, () => {
+      console.log('Deleted all documents in Collection');
 
-        // save multiple documents to the collection referenced by Job Model
-        Job.collection.insertMany(jobsWithAgencyData, function (err, docs) {
-          if (err){
-              return console.error(err);
-          } else {
-            console.log("Multiple documents inserted to Collection");
-          }
-        });
-      })
-  })
-})
+      // save multiple documents to the collection referenced by Job Model
+      Job.collection.insertMany(jobsWithAgencyData, (err, docs) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log('Multiple documents inserted to Collection');
+      });
+    });
+
+    const Meta = mongoose
+      .model('Meta', mongoose.Schema({ dataUpdatedAt: String }), 'meta');
+
+    Meta.create({ dataUpdatedAt }, function (err, small) {
+      if (err) return handleError(err);
+      console.log('Saved Metadata!');
+    });
+  });
+
+})();
